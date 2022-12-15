2275bcc569f873a51426b1e1c9639fe88f946fd1
Ana Rouze <anarouze10@gmail.com>
2022-12-15 18:50:26 -0300

    Exemplos de variaveis e operadores java

--- TiposVariaveis.java/src/Operadores2.java
+++ TiposVariaveis.java/src/Operadores2.java
@@ -1,7 +1,7 @@
 public class Operadores2 {
     public static void main(String[] args){
         int numero = 5;
-        //x repetições
+        //i repetições
         numero++;
 
         System.out.println(numero);
